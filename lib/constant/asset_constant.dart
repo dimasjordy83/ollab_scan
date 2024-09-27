a627a058e3c0c72f06210896e9faa887e4978d68
dimasjordy83 <dimasjordy.12.9c.2017@gmail.com>
2024-09-27 12:50:22 +0700

    commit 1

--- lib/constant/asset_constant.dart
+++ lib/constant/asset_constant.dart
@@ -1,31 +1,76 @@
-
 class AssetsHandler {
   static Map<String, String> getResultAssets(String result) {
     String src;
     String audio;
-
+    String title;
     switch (result) {
+      case "cooking":
+      case "paint":
+      case "reading":
+      case "solving":
+      case "watching":
+        src = "assets/model/$result.glb";
+        audio = "audio/$result.mp3";
+        title = "Task 8 Fill and Write";
+        break;
       case "number":
         src = "assets/model/number.glb";
         audio = "audio/number.mp3";
+        title = "Task 3 Listen and Repeat";
         break;
-      case "listening 1":
-        src = "assets/model/talk1.glb";
-        audio = "audio/talk1.mp3";
+      case "badroom":
+      case "bathroom":
+      case "dining room":
+      case "garage":
+      case "kitchen":
+        src = "assets/model/$result.glb";
+        audio = "audio/$result.mp3";
+        title = "Lesson";
         break;
-      case "listening 2":
-        src = "assets/model/talk2.glb";
-        audio = "audio/talk2.mp3";
+      case "capter4_brush":
+      case "capter4_cooking":
+      case "capter4_eating":
+      case "capter4_reading":
+      case "capter4_sleeping":
+        src = "assets/model/$result.glb";
+        audio = "audio/$result.mp3";
+        title = "Simple Present Tense";
+        break;
+      case "bookshelf":
+      case "lamp":
+      case "model":
+      case "sit":
+      case "table":
+      case "television":
+        src = "assets/model/$result.glb";
+        audio = "audio/$result.mp3";
+        title = "Task 6 Listen and Say";
+        break;
+      case "dippers":
+      case "shower":
+      case "soap":
+      case "toothbrush":
+      case "toothpaste":
+        src = "assets/model/$result.glb";
+        audio = "audio/$result.mp3";
+        title = "Task 2 Say a chant";
+        break;
+      case "cook-water":
+      case "cooking_egg":
+      case "cutting":
+      case "mopping":
+      case "sweep":
+        src = "assets/model/$result.glb";
+        audio = "audio/$result.mp3";
+        title = "Task 1 Say out Loud";
         break;
       default:
-        src = "assets/model/cooking.glb";
-        audio = "audio/talk1.mp3";
+        src = "assets/model/default.glb";
+        audio = "audio/default.mp3";
+        title = "Default";
         break;
     }
 
-    return {
-      'src': src,
-      'audio': audio,
-    };
+    return {'src': src, 'audio': audio, 'title': title};
   }
 }
--- lib/view/result.dart
+++ lib/view/result.dart
@@ -17,12 +17,34 @@ class ResultView extends StatelessWidget {
     Map<String, String> assets = AssetsHandler.getResultAssets(result);
     String src = assets['src']!;
     String audio = assets['audio']!;
+    String title = assets['title']!;
     final AudioPlayer audioPlayer = AudioPlayer();
+
+    // Function to get the display name for the model
+    String getDisplayName(String modelName) {
+      Map<String, String> replacements = {
+        'paint': 'drawing',
+        'number': 'task 3',
+        'capter4_cooking': 'My mother cooks meal in the kitchen',
+        'capter4_reading': 'Do the read books in the living room?',
+        'capter4_sleeping': 'Adi does not sleep in the living room',
+        'capter4_eating': 'We eat fried rice in the dining room',
+        'sit': 'sofa',
+        'table': 'Coffee Table',
+        'cook-water': 'i can boil water',
+        'cooking-egg': 'I can fry an egg',
+        'cutting': 'I can cut vegetables',
+        'mopping': 'I can mop the floor',
+        'sweep': 'I can sweep the floor',
+      };
+      return replacements[modelName] ?? modelName;
+    }
+
     return PopScope(
       canPop: false,
       child: Scaffold(
           appBar: AppBar(
-            title: Text("title"),
+            title: Text(title),
             centerTitle: true,
             leading: IconButton(
                 onPressed: () {
@@ -36,6 +58,21 @@ class ResultView extends StatelessWidget {
           ),
           body: Stack(
             children: [
+              // Display model name
+              Positioned(
+                top: 10,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Text(
+                    getDisplayName(result),
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
               ModelViewer(
                 src: src,
                 ar: true,
